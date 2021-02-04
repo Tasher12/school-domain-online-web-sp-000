4d9f7ed9f015eac8defb658ac6a159e4aa952df2
authentic-pipe-8414 <tasher.stidham@outlook.com>
2021-02-04 18:30:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_accessor :school, :roster, :grade, :name
+  
+  
+  def initialize(roster)
+    @roster = Hash.new {|h,k| h[k] = []}
+  end 
+  
+ def add_student(name, grade)
+   @name = name
+   @grade = grade 
+   roster[grade] << name 
+  end 
+   
+  def grade(grade)
+    
+  end
+ 
+
+
